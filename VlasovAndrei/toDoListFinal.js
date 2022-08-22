9200f7a9822dd265ca2d7343b24efab349fd42fd
callBackLater <a.a.vlasov88@gmail.com>
2022-08-22 16:20:35 +0500

    dash-string added to showList() for statuses with no tasks

--- VlasovAndrei/toDoListFinal.js
+++ VlasovAndrei/toDoListFinal.js
@@ -1,14 +1,14 @@
-const list = {
-    'make homework': 'In Progress',
-    'have a meal': 'To Do',
-    'feed the cat': 'Done',
-    'learning JS': 'In Progress',
-}
-
 const statuses = {
     TODO: 'To Do',
     INPROGRESS: 'In Progress',
     DONE: 'Done',
+}
+
+const list = {
+    'make homework': statuses.INPROGRESS,
+    'have a meal': statuses.TODO,
+    'feed the cat': statuses.DONE,
+    'learning JS': statuses.INPROGRESS,
 }
 
 function addTask(task) {
@@ -38,20 +38,27 @@ function deleteTask(task) {
 function showList() {
     let resultList = ``;
     for (let key in statuses) {
+        let isProps = false;
         resultList += `${statuses[key]}:\n`;
         for (let prop in list) {
             if (list[prop] == statuses[key]) {
                 resultList += `\t${prop}\n`;
+                isProps = true;
             }
+        }
+        if (isProps == false) {
+            resultList +='\t-';
         }
     }
     console.log(resultList);
 }
+
 
 addTask('watch TV');
 changeStatus('have a meal', 'In Progres'); //без двойной 's'
 changeStatus('have a meal', 'In Progress');
 changeStatus('go shopping', 'Done');
 deleteTask('make homework');
+changeStatus('feed the cat', 'In Progress');
 showList();
 console.log(list);
